f4b3b1a1da54f53e0b9b37443bc14e2bff453c28
hristo2612 <hristoapps@gmail.com>
2019-06-17 17:35:10 +0300

    Add some basic stuff

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(private storage: Storage) {}
+
+  ngOnInit() {
+    this.storage.get('counters').then((counters) => {
+      if (counters) {
+
+      }
+    });
+  }
 
 }
