58d8f1505804df86e9ef7b008dcccc74d74ffff5
hristo2612 <hristoapps@gmail.com>
2019-08-16 00:54:24 +0300

    Add challenges

--- src/app/shared/challenges.ts
+++ src/app/shared/challenges.ts
@@ -75,4 +75,48 @@ export const challenges = [
         subtitle: 'Practice asking questions',
         description: 'What would you like to ask? Don\'t be generic'
     },
+    {
+        subtitle: 'Be aware of different levels of comfort',
+        description: 'Try starting small — share only minor things with strangers or acquaintances. And if opening up is new for you, you might start out by sharing minor things with close friends, too'
+    },
+    {
+        subtitle: 'Try to talk to friends and family more often',
+        description: 'If you are having trouble finding the motivation to do this, try keeping in mind that having social support can reduce stress and can just be overall healthy for you.'
+    },
+    {
+        subtitle: 'Embrace vulnerability',
+        description: 'One way to become more comfortable with vulnerability is to separate your action from the outcome. You can control what you say and whom you open up to, but you can\'t control their reaction or what they do. Once you realize that you aren\'t responsible for how others behave or react, you may feel more free to open up'
+    },
+    {
+        subtitle: 'Try journaling',
+        description: 'Try setting aside five to 10 minutes every day to journal. Don\'t worry about making sense or your handwriting or if what you\'re writing seems silly or petty or stupid.[8] Just make the commitment to express yourself every day.'
+    },
+    {
+        subtitle: 'Initiate conversations',
+        description: 'Remember to listen carefully to what the person says to you, as this will give you clues about how to follow-up the conversation'
+    },
+    {
+        subtitle: 'Set the bar higher',
+        description: 'If you have talked to 3 people today, go to the local supermarket and initiate a conversation with the first person you see'
+    },
+    {
+        subtitle: 'Ask open-ended questions',
+        description: '"What\'s something wonderful that\'s happened to you lately?" or, "What activities would you recommend in town?"'
+    },
+    {
+        subtitle: 'Try saying "Yes" more',
+        description: 'If you get invited to some social gathering today or whenever, and you feel hesitant, just say yes!'
+    },
+    {
+        subtitle: 'Pretend you’re a tourist in your hometown',
+        description: 'Few people have opened up to all the possibilities in their region.'
+    },
+    {
+        subtitle: 'Show support to them when they open up',
+        description: 'Don\'t interrupt them. Try to focus on them and their situation until they feel better. '
+    },
+    {
+        subtitle: 'Connect to an old friend',
+        description: 'It might be through social media or on the phone. Just ask them how are they, what have they been up to?'
+    }
 ];
